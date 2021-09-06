c8f559ba24b6f7b81b76632341ec077193bd9512
pullitdown <595259789@qq.com>
2021-09-06 15:47:38 +0800

    new file

--- automatic differentiation.py
+++ automatic differentiation.py
@@ -1,3 +1,28 @@
 #conding=gbk
 import torch
 
+a=torch.arange(4.0)
+a.requires_grad_(True)
+print(a.grad)
+b=torch.arange(4,8,dtype=torch.float32)
+b.requires_grad_(True)
+y1=torch.dot(b,b)
+y1.backward()
+print(b.grad)
+y=2*torch.dot(a,b)
+print(a.grad)
+
+print(y)
+y.backward()
+print(a.grad)
+print(b.grad)
+
+a.grad.zero_()
+y2=a.sum()
+y2.backward()
+print(a.grad)
+
+a.grad.zero_()
+y2=torch.dot(a,a)
+y2.backward()#only trough backward the grad in x would be add up
+print(a.grad)
---
+++ 概率.py
@@ -0,0 +1,8 @@
+
+#coding=gbk
+import torch
+from torch.distributions import multinomial
+from d2l import torch as d2l
+
+one=torch.ones([6])
+print(one)
